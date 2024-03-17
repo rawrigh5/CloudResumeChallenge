9bd33435314e663e379f3358d3b9c5c5898f0e7d
rawrigh5 <rockywright2015@gmail.com>
2024-03-17 18:48:29 -0500

    Massive amount or organizational changes to the HTML and included information to the about me page. Also making my first attempt at the javascript to display and hide content depending on the page selection

--- CloudResume/resumeFunctions.js
+++ CloudResume/resumeFunctions.js
@@ -1,1 +1,16 @@
-cloud reume
+<script>
+    document.querySelectorAll('nav a').forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent default navigation behavior
+
+            var pageId = this.getAttribute('href').substring(1); // Get the target page ID
+            var currentPage = document.querySelector('.page:not(.hidden)'); // Get the currently visible page
+
+            if (currentPage) {
+                currentPage.classList.add('hidden'); // Hide the currently visible page
+            }
+
+            document.getElementById(pageId).classList.remove('hidden'); // Show the target page
+        });
+    });
+</script>
